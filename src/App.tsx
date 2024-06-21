06a53d273006139342c6925f789fe821877505ae
rincondeveloper <4braham3mma@gmail.com>
2024-06-20 21:05:45 -0600

    add chadcn/ui and setting firs routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import './App.css'
-import { Button } from './components/ui/button'
+// import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp/SignUp'
+import Login from './pages/Login/Login'
 
 function App() {
-
   return (
-    <>
-      <Button variant="destructive">Primary</Button>
-    </>
+    <Routes>
+      {/* <Route path="/" element={<div>Landing Page</div>} /> */}
+      {/* path: "/" is for landing page */}
+
+      {/* path: "/signup" is for signup page. "/" Is momentary */}
+      <Route path="/" element={<SignUp />} /> 
+      <Route path="/login" element={<Login />} />
+
+    </Routes>
   )
 }
 
